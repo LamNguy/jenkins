3edf0eb0ad8813ba09a140de3cf36ce7dad1cd02
LamNguy <lamchipabc@gmail.com>
2023-04-12 14:05:39 +0700

    update

--- install_Cortex_Linux.sh
+++ install_Cortex_Linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo mkdir -p /etc/panw
-sudo tar xf /home/cloud/Cortex_linux_rpm.tar.gz
-sudo cd /home/cloud/
+sudo tar xf /root/Cortex_linux_rpm.tar.gz
+sudo cd /root/
 sudo cp cortex.conf /etc/panw/
 sudo yum -y install ./cortex-*.rpm
